631c1e9bbac8349a68851840007d26a302e35445
Vicente Cano <vecano@gmail.com>
2015-07-09 16:51:34 -0500

    Add examples for inserting and retreiving mongodb documents

--- mongodb/src/mongodb/core.clj
+++ mongodb/src/mongodb/core.clj
@@ -1,5 +1,15 @@
 (ns mongodb.core
-  (:gen-class))
+  (:gen-class)
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]))
+
+(let [conn (mg/connect)
+      db   (mg/get-db conn "monger-test")
+      coll "documents"]
+  ; (mc/insert db coll {:first_name "John"  :last_name "Lennon"})
+  ; (mc/insert db coll {:first_name "Ringo" :last_name "Starr"})
+  (println "--------------------------------------------------------------------------------")
+  (println (mc/count db coll { :last_name "Lennon"})))
 
 (defn -main
   "I don't do a whole lot ... yet."
